741e6c32a1ba1de9a31fcf9b6932d677b6841bc0
Carl J. Bialorucki <cbialo2@outlook.com>
2022-02-03 23:19:18 -0700

    Added content.

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,9 +1,16 @@
 document.querySelector("#copyNotice").innerHTML = `&copy; ${new Date().getFullYear()} Rexburg Chamber of Commerce | Carl J. Bialorucki | WDD 230 Project | Last Updated: ${new Date(document.lastModified).toLocaleString()}`;
 
-const currentDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(new Date());
-document.querySelector("#dateDisplay").innerHTML = currentDate;
+const currentDate = new Date();
+document.querySelector("#dateDisplay").innerHTML = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(currentDate);
 
 const menuButton = document.getElementsByClassName("navList")[0];
+
+if (currentDate.getDay() == 1 || currentDate.getDay() == 2){
+    let banner = document.createElement('p');
+    banner.innerHTML = "Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    banner.setAttribute('id', 'banner');
+    document.querySelector("header").appendChild(banner);
+}
 
 function toggleNavMenu(){
     menuButton.classList.toggle("showList");
