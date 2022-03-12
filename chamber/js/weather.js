a31470d14fb6f889c489ce80b99e9ae45e243626
Carl J. Bialorucki <cbialo2@outlook.com>
2022-03-11 21:52:51 -0700

    Updated to use https for weather API

--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -1,8 +1,8 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?zip=83440,us&units=imperial&appid=6ab85a365f86c338d15b0ecfee11c84e";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?zip=83440,us&units=imperial&appid=6ab85a365f86c338d15b0ecfee11c84e";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    const iconURI = `http://openweathermap.org/img/wn/${jsObject["weather"][0].icon}@2x.png`;
+    const iconURI = `https://openweathermap.org/img/wn/${jsObject["weather"][0].icon}@2x.png`;
     document.getElementById("weatherIcon").setAttribute('src', iconURI);
     document.getElementById("spanTemp").innerText = Math.round(jsObject["main"].temp);
     document.getElementById("weatherCondition").innerText = jsObject["weather"][0].description;
